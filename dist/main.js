a26acb6b2241fb9b527343a77ed9944eabd3b498
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-12-24 20:45:04 +0200

    update environments

--- dist/main.js
+++ dist/main.js
@@ -1398,22 +1398,22 @@ const MENUITEMS = [
     { state: 'courses', name: 'Courses', type: 'link', icon: 'school' },
     { state: 'subscriptions', name: 'Subscriptions', type: 'link', icon: 'money' },
     { state: 'requests', name: 'Requests', type: 'link', icon: 'assistant' },
-    { state: 'button', type: 'link', name: 'Buttons', icon: 'crop_7_5' },
-    { state: 'grid', type: 'link', name: 'Grid List', icon: 'view_comfy' },
-    { state: 'lists', type: 'link', name: 'Lists', icon: 'view_list' },
-    { state: 'menu', type: 'link', name: 'Menu', icon: 'view_headline' },
-    { state: 'tabs', type: 'link', name: 'Tabs', icon: 'tab' },
-    { state: 'stepper', type: 'link', name: 'Stepper', icon: 'web' },
-    { state: 'expansion', type: 'link', name: 'Expansion Panel', icon: 'vertical_align_center' },
-    { state: 'chips', type: 'link', name: 'Chips', icon: 'vignette' },
-    { state: 'toolbar', type: 'link', name: 'Toolbar', icon: 'voicemail' },
-    { state: 'progress-snipper', type: 'link', name: 'Progress snipper', icon: 'border_horizontal' },
-    { state: 'progress', type: 'link', name: 'Progress Bar', icon: 'blur_circular' },
-    { state: 'dialog', type: 'link', name: 'Dialog', icon: 'assignment_turned_in' },
-    { state: 'tooltip', type: 'link', name: 'Tooltip', icon: 'assistant' },
-    { state: 'snackbar', type: 'link', name: 'Snackbar', icon: 'adb' },
-    { state: 'slider', type: 'link', name: 'Slider', icon: 'developer_mode' },
-    { state: 'slide-toggle', type: 'link', name: 'Slide Toggle', icon: 'all_inclusive' }
+    // { state: 'button', type: 'link', name: 'Buttons', icon: 'crop_7_5' },
+    // { state: 'grid', type: 'link', name: 'Grid List', icon: 'view_comfy' },
+    // { state: 'lists', type: 'link', name: 'Lists', icon: 'view_list' },
+    // { state: 'menu', type: 'link', name: 'Menu', icon: 'view_headline' },
+    // { state: 'tabs', type: 'link', name: 'Tabs', icon: 'tab' },
+    // { state: 'stepper', type: 'link', name: 'Stepper', icon: 'web' },
+    // {state: 'expansion', type: 'link', name: 'Expansion Panel', icon: 'vertical_align_center'},
+    // { state: 'chips', type: 'link', name: 'Chips', icon: 'vignette' },
+    // { state: 'toolbar', type: 'link', name: 'Toolbar', icon: 'voicemail' },
+    // {state: 'progress-snipper', type: 'link', name: 'Progress snipper', icon: 'border_horizontal'},
+    // {state: 'progress', type: 'link', name: 'Progress Bar', icon: 'blur_circular'},
+    // {state: 'dialog', type: 'link', name: 'Dialog', icon: 'assignment_turned_in'},
+    // { state: 'tooltip', type: 'link', name: 'Tooltip', icon: 'assistant' },
+    // { state: 'snackbar', type: 'link', name: 'Snackbar', icon: 'adb' },
+    // { state: 'slider', type: 'link', name: 'Slider', icon: 'developer_mode' },
+    // {state: 'slide-toggle', type: 'link', name: 'Slide Toggle', icon: 'all_inclusive'}
 ];
 class MenuItems {
     getMenuitem() {
@@ -1678,7 +1678,8 @@ __webpack_require__.r(__webpack_exports__);
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 const environment = {
     production: false,
-    baseUrl: 'http://localhost:4000'
+    // baseUrl: 'http://localhost:4000'
+    baseUrl: 'https://brave-ihmy.onrender.com'
 };
 
 
